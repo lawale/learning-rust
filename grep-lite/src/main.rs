71b46018e752dbed9aac55193dc27b65d949f10b
Olawale Lawal <olawale.lawal@kuda.com>
2024-04-14 13:18:06 +0100

    implement functionality to grep receive file path as argument

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use regex::Regex;
 use clap::{Command, Arg};
 
@@ -7,26 +9,42 @@ fn main() {
         .about("searches for pattern")
         .version("0.1.0")
         .arg(Arg::new("pattern")
+            .long("pattern")
+            .short('p')
             .help("The pattern to search for")
+            .required(true))
+        .arg(Arg::new("file")
+            .long("file")
+            .short('f')
+            .help("The input file to search")
             .required(false))
         .get_matches();
 
-    let pattern: &String;
-    match matches.get_one("pattern") {
-        Some(p) => pattern = p,
-        None => panic!("Pattern not found")
-    }
+    let std_flag = &"-".to_string();
+
+    let file_name = matches.get_one("file").unwrap_or(std_flag);
+
+    let pattern: &String = matches.get_one("pattern").unwrap_or_else(|| panic!("Pattern Required"));
 
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "\
+    if file_name == "-" {
+        let stdin = std::io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    }else {
+        let file = File::open(file_name).unwrap_or_else(|_| panic!("File Not Found"));
 
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+        let reader = BufReader::new(file);
 
-    for line in quote.lines() {
-        let contain_substring = re.find(line);
+        process_lines(reader, re);
+    }
+}
+
+fn process_lines<T: BufRead + Sized>(reader: T, regex: Regex) {
+    for _line in reader.lines(){
+        let line = _line.unwrap();
+        let contain_substring = regex.find(&line);
         match contain_substring {
             Some(_) => {
                 println!("{}", line);
